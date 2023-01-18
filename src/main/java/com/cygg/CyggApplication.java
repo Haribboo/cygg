b0a15a9f695b0dc136df5bd30f735a8827f2055c
Haribboo <choisejun99_@naver.com>
2023-01-18 17:03:52 +0900

    first init
    프로젝트 초기 셋팅

---
+++ src/main/java/com/cygg/CyggApplication.java
@@ -0,0 +1,13 @@
+package com.cygg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyggApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cygg/CyggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cygg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
